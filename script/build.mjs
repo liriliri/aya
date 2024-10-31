1fee7de62907c425b354809a6d080298d85068ec
surunzi <surunzi@foxmail.com>
2024-11-01 00:10:08 +0800

    feat: windows build

---
+++ script/build.mjs
@@ -0,0 +1,24 @@
+const pkg = await fs.readJson('package.json')
+const electron = pkg.devDependencies.electron
+delete pkg.devDependencies
+pkg.devDependencies = {
+  electron,
+}
+delete pkg.scripts
+pkg.scripts = {
+  start: 'electron main/index.js',
+}
+pkg.main = 'main/index.js'
+
+await $`npm run build:main`
+await $`npm run build:preload`
+await $`npm run build:renderer`
+
+await fs.copy('build', 'dist/build')
+cd('dist')
+
+await fs.writeJson('package.json', pkg, {
+  spaces: 2,
+})
+
+await $`npm i`
---
+++ script/pack.mjs
@@ -0,0 +1,26 @@
+import builder from 'electron-builder'
+
+cd('dist')
+
+const pkg = await fs.readJson('package.json')
+
+const config = {
+  directories: {
+    output: `../release/${pkg.version}`,
+  },
+  files: ['main', 'preload', 'renderer'],
+  win: {
+    target: [
+      {
+        target: 'nsis',
+      },
+    ],
+  },
+  mac: {
+    electronLanguages: ['zh_CN', 'en'],
+  },
+}
+
+await builder.build({
+  config,
+})

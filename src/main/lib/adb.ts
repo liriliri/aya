2abe47646d10f6a74006defc72219afe044fbbcf
redhoodsu <redhoodsu@tencent.com>
2024-12-04 13:13:17 +0800

    refactor: adb shell

--- src/main/lib/adb.ts
+++ src/main/lib/adb.ts
@@ -4,9 +4,7 @@ import { resolveUnpack, handleEvent } from './util'
 import map from 'licia/map'
 import types from 'licia/types'
 import filter from 'licia/filter'
-import Emitter from 'licia/Emitter'
 import isStrBlank from 'licia/isStrBlank'
-import uniqId from 'licia/uniqId'
 import each from 'licia/each'
 import singleton from 'licia/singleton'
 import trim from 'licia/trim'
@@ -24,6 +22,8 @@ import axios from 'axios'
 import * as base from './adb/base'
 import { shell, getPidNames, getProcesses } from './adb/base'
 import * as logcat from './adb/logcat'
+import * as shellAdb from './adb/shell'
+import { createShell, writeShell, resizeShell, killShell } from './adb/shell'
 import {
   openLogcat,
   closeLogcat,
@@ -299,163 +299,6 @@ async function getStorage(deviceId: string) {
   }
 }
 
-class ShellProtocol {
-  static STDIN = 0
-  static STDOUT = 1
-  static STDERR = 2
-  static EXIT = 3
-  static CLOSE_STDIN = 4
-  static WINDOW_SIZE_CHANGE = 5
-
-  static encodeData(id, data) {
-    data = Buffer.from(data, 'utf8')
-    const buf = Buffer.alloc(5 + data.length)
-    buf.writeUInt8(id, 0)
-    buf.writeUInt32LE(data.length, 1)
-    data.copy(buf, 5)
-
-    return buf
-  }
-
-  static decodeData(buf) {
-    const result: Array<{
-      id: number
-      data: Buffer
-    }> = []
-
-    for (let i = 0, len = buf.length; i < len; ) {
-      const id = buf.readUInt8(i)
-      const len = buf.readUInt32LE(i + 1)
-      const data = buf.slice(i + 5, i + 5 + len)
-      result.push({
-        id,
-        data,
-      })
-      i += 5 + len
-    }
-
-    return result
-  }
-}
-
-class Protocol {
-  static OKAY = 'OKAY'
-
-  static encodeLength(length) {
-    return length.toString(16).padStart(4, '0').toUpperCase()
-  }
-
-  static encodeData(data) {
-    const len = Protocol.encodeLength(data.length)
-    return Buffer.concat([Buffer.from(len), data])
-  }
-}
-
-class AdbPty extends Emitter {
-  private connection: any
-  private useV2 = true
-  constructor(connection: any) {
-    super()
-
-    this.connection = connection
-  }
-  async init(useV2 = true) {
-    const { connection } = this
-
-    this.useV2 = useV2
-    const protocol = useV2 ? 'shell,v2:' : 'shell:'
-    connection.write(Protocol.encodeData(Buffer.from(protocol)))
-    const result = await connection.parser.readAscii(4)
-    if (result !== Protocol.OKAY) {
-      throw new Error('Failed to create shell')
-    }
-
-    if (useV2) {
-      const { socket } = connection
-      socket.on('readable', () => {
-        const buf = socket.read()
-        if (buf) {
-          const packets = ShellProtocol.decodeData(buf)
-          for (let i = 0, len = packets.length; i < len; i++) {
-            const { id, data } = packets[i]
-            if (id === ShellProtocol.STDOUT) {
-              this.emit('data', data.toString('utf8'))
-            }
-          }
-        }
-      })
-    } else {
-      const { socket } = connection
-      socket.on('readable', () => {
-        const buf = socket.read()
-        if (buf) {
-          this.emit('data', buf.toString('utf8'))
-        }
-      })
-    }
-  }
-  resize(cols: number, rows: number) {
-    if (this.useV2) {
-      this.connection.socket.write(
-        ShellProtocol.encodeData(
-          ShellProtocol.WINDOW_SIZE_CHANGE,
-          Buffer.from(`${rows}x${cols},0x0\0`)
-        )
-      )
-    }
-  }
-  write(data: string) {
-    if (this.useV2) {
-      this.connection.socket.write(
-        ShellProtocol.encodeData(ShellProtocol.STDIN, Buffer.from(data))
-      )
-    } else {
-      this.connection.socket.write(Buffer.from(data))
-    }
-  }
-  kill() {
-    this.connection.end()
-  }
-}
-
-const ptys: types.PlainObj<AdbPty> = {}
-
-async function createShell(deviceId: string) {
-  const device = await client.getDevice(deviceId)
-
-  const transport = await device.transport()
-  let adbPty = new AdbPty(transport)
-  try {
-    await adbPty.init()
-    /* eslint-disable @typescript-eslint/no-unused-vars */
-  } catch (e) {
-    adbPty.kill()
-    const transport = await device.transport()
-    adbPty = new AdbPty(transport)
-    await adbPty.init(false)
-  }
-  const sessionId = uniqId('shell')
-  adbPty.on('data', (data) => {
-    window.sendTo('main', 'shellData', sessionId, data)
-  })
-  ptys[sessionId] = adbPty
-
-  return sessionId
-}
-
-async function writeShell(sessionId: string, data: string) {
-  ptys[sessionId].write(data)
-}
-
-async function resizeShell(sessionId: string, cols: number, rows: number) {
-  ptys[sessionId].resize(cols, rows)
-}
-
-async function killShell(sessionId: string) {
-  ptys[sessionId].kill()
-  delete ptys[sessionId]
-}
-
 const getPackages = singleton(async (deviceId: string) => {
   const result: string = await shell(deviceId, 'pm list packages')
 
@@ -556,6 +399,7 @@ export async function init() {
 
   base.setClient(client)
   logcat.setClient(client)
+  shellAdb.setClient(client)
 
   function onDeviceChange() {
     setTimeout(() => window.sendTo('main', 'changeDevice'), 2000)
---
+++ src/main/lib/adb/shell.ts
@@ -0,0 +1,172 @@
+import types from 'licia/types'
+import Emitter from 'licia/Emitter'
+import uniqId from 'licia/uniqId'
+import * as window from '../window'
+import { Client } from '@devicefarmer/adbkit'
+
+let client: Client
+
+class ShellProtocol {
+  static STDIN = 0
+  static STDOUT = 1
+  static STDERR = 2
+  static EXIT = 3
+  static CLOSE_STDIN = 4
+  static WINDOW_SIZE_CHANGE = 5
+
+  static encodeData(id, data) {
+    data = Buffer.from(data, 'utf8')
+    const buf = Buffer.alloc(5 + data.length)
+    buf.writeUInt8(id, 0)
+    buf.writeUInt32LE(data.length, 1)
+    data.copy(buf, 5)
+
+    return buf
+  }
+
+  static decodeData(buf) {
+    const result: Array<{
+      id: number
+      data: Buffer
+    }> = []
+
+    for (let i = 0, len = buf.length; i < len; ) {
+      const id = buf.readUInt8(i)
+      const len = buf.readUInt32LE(i + 1)
+      const data = buf.slice(i + 5, i + 5 + len)
+      result.push({
+        id,
+        data,
+      })
+      i += 5 + len
+    }
+
+    return result
+  }
+}
+
+class Protocol {
+  static OKAY = 'OKAY'
+
+  static encodeLength(length) {
+    return length.toString(16).padStart(4, '0').toUpperCase()
+  }
+
+  static encodeData(data) {
+    const len = Protocol.encodeLength(data.length)
+    return Buffer.concat([Buffer.from(len), data])
+  }
+}
+
+class AdbPty extends Emitter {
+  private connection: any
+  private useV2 = true
+  constructor(connection: any) {
+    super()
+
+    this.connection = connection
+  }
+  async init(useV2 = true) {
+    const { connection } = this
+
+    this.useV2 = useV2
+    const protocol = useV2 ? 'shell,v2:' : 'shell:'
+    connection.write(Protocol.encodeData(Buffer.from(protocol)))
+    const result = await connection.parser.readAscii(4)
+    if (result !== Protocol.OKAY) {
+      throw new Error('Failed to create shell')
+    }
+
+    if (useV2) {
+      const { socket } = connection
+      socket.on('readable', () => {
+        const buf = socket.read()
+        if (buf) {
+          const packets = ShellProtocol.decodeData(buf)
+          for (let i = 0, len = packets.length; i < len; i++) {
+            const { id, data } = packets[i]
+            if (id === ShellProtocol.STDOUT) {
+              this.emit('data', data.toString('utf8'))
+            }
+          }
+        }
+      })
+    } else {
+      const { socket } = connection
+      socket.on('readable', () => {
+        const buf = socket.read()
+        if (buf) {
+          this.emit('data', buf.toString('utf8'))
+        }
+      })
+    }
+  }
+  resize(cols: number, rows: number) {
+    if (this.useV2) {
+      this.connection.socket.write(
+        ShellProtocol.encodeData(
+          ShellProtocol.WINDOW_SIZE_CHANGE,
+          Buffer.from(`${rows}x${cols},0x0\0`)
+        )
+      )
+    }
+  }
+  write(data: string) {
+    if (this.useV2) {
+      this.connection.socket.write(
+        ShellProtocol.encodeData(ShellProtocol.STDIN, Buffer.from(data))
+      )
+    } else {
+      this.connection.socket.write(Buffer.from(data))
+    }
+  }
+  kill() {
+    this.connection.end()
+  }
+}
+
+const ptys: types.PlainObj<AdbPty> = {}
+
+export async function createShell(deviceId: string) {
+  const device = await client.getDevice(deviceId)
+
+  const transport = await device.transport()
+  let adbPty = new AdbPty(transport)
+  try {
+    await adbPty.init()
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+  } catch (e) {
+    adbPty.kill()
+    const transport = await device.transport()
+    adbPty = new AdbPty(transport)
+    await adbPty.init(false)
+  }
+  const sessionId = uniqId('shell')
+  adbPty.on('data', (data) => {
+    window.sendTo('main', 'shellData', sessionId, data)
+  })
+  ptys[sessionId] = adbPty
+
+  return sessionId
+}
+
+export async function writeShell(sessionId: string, data: string) {
+  ptys[sessionId].write(data)
+}
+
+export async function resizeShell(
+  sessionId: string,
+  cols: number,
+  rows: number
+) {
+  ptys[sessionId].resize(cols, rows)
+}
+
+export async function killShell(sessionId: string) {
+  ptys[sessionId].kill()
+  delete ptys[sessionId]
+}
+
+export function setClient(c: Client) {
+  client = c
+}

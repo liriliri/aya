e27ad0e8a9df26a98260c29314267d7dac421244
surunzi <surunzi@foxmail.com>
2025-03-30 21:27:39 +0800

    chore: small changes

--- script/pack.mjs
+++ script/pack.mjs
@@ -29,7 +29,7 @@ const config = {
   publish: {
     provider: 'generic',
     url: 'https://release.liriliri.io/',
-    channel: '${productName}-latest-${arch}',
+    channel: '${productName}-latest',
   },
 }
 

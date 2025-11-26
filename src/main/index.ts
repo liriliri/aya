bcc1e4a81a2ff23f9549eb570bcadcf9f087f46c
surunzi <surunzi@foxmail.com>
2025-11-26 08:16:17 +0800

    chore: small changes

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,35 +1,19 @@
 import { app } from 'electron'
 import * as menu from './lib/menu'
-import * as ipc from 'share/main/lib/ipc'
 import * as main from './window/main'
-import * as language from 'share/main/lib/language'
-import * as theme from 'share/main/lib/theme'
 import * as adb from './lib/adb'
 import * as terminal from 'share/main/window/terminal'
-import * as updater from 'share/main/lib/updater'
-import { setupTitlebar } from 'custom-electron-titlebar/main'
 import log from 'share/common/log'
+import 'share/main'
 
 const logger = log('main')
 logger.info('start')
 
-if (!app.requestSingleInstanceLock()) {
-  app.quit()
-  process.exit(0)
-}
-
-app.setName('Aya')
-
 app.on('ready', () => {
   logger.info('app ready')
 
-  setupTitlebar()
   terminal.init()
-  language.init()
-  theme.init()
   adb.init()
-  ipc.init()
   main.showWin()
   menu.init()
-  updater.init()
 })
